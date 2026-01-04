bdb6869eda9433110b1a55c442c9c1eb4d5328da
raunak singh <raunaksingh@raunaks-MacBook-Air.local>
2026-01-05 01:10:28 +0530

    update db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose"
 import {ENV} from "./env.js"
 export const connectDB=async()=>{
     try {
+        if(!ENV.DB_URL){
+            throw new Error("DB url is not defined in env variable");
+        }
         const conn=await mongoose.connect(ENV.DB_URL)
         console.log("✅ Connected to Mongodb:",conn.connection.host)
     } catch (error) {
